ac1996b732bce04d205259a722b5ae07f000fd0a
Khanh Van <vdongkhanh@gmail.com>
2021-06-30 21:50:03 +0300

    Added endpoint for creating and authenticating user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
         email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
 
-        self.assertTrue(user.email, email.lower())
+        self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
         with self.assertRaises(ValueError):
